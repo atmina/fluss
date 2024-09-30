7bba9705bf7cf9cad7b371bd767129f116211669
mvarendorff <michel.vonvarendorff@atmina.de>
2024-09-30 10:04:38 +0200

    fix: expose GetAggregateRoot()

--- src/Fluss/UnitOfWork/IWriteUnitOfWork.cs
+++ src/Fluss/UnitOfWork/IWriteUnitOfWork.cs
@@ -7,6 +7,9 @@ public interface IWriteUnitOfWork : IUnitOfWork
 {
     ValueTask<TAggregate> GetAggregate<TAggregate, TKey>(TKey key)
         where TAggregate : AggregateRoot<TKey>, new();
+    
+    ValueTask<TAggregate> GetAggregate<TAggregate>()
+        where TAggregate : AggregateRoot, new();
 
     ValueTask Publish(Event @event, AggregateRoot? aggregate = null);
 }
