2b0b12f4761039716d933fcb0f1b0f1c3522a0bd
mvarendorff <michel.vonvarendorff@atmina.de>
2024-11-04 15:50:09 +0100

    chore: revert Bench

--- src/Benchmark/Program.cs
+++ src/Benchmark/Program.cs
@@ -1,22 +1,4 @@
 ﻿using Benchmark;
 using BenchmarkDotNet.Running;
-using Fluss;
-using Fluss.Authentication;
-using Microsoft.Extensions.DependencyInjection;
 
-var sc = new ServiceCollection();
-sc.AddEventSourcing();
-sc.AddPolicy<AllowAllPolicy>();
-
-var sp = sc.BuildServiceProvider();
-for (var i = 0; i < 1500; i++)
-{
-    await sp.GetSystemUserUnitOfWorkFactory().Commit(async uow =>
-    {
-        await uow.Publish(new TestEvent(i));
-    });
-}
-
-await Task.Delay(3000);
-
-// BenchmarkRunner.Run<Bench>();
+BenchmarkRunner.Run<Bench>();
